e19eb0697c407ff35fd9f8aa7a665d47c1ab9c21
HarleyGwak <351696132@qq.com>
2015-06-26 16:09:00 +0900

    graph finish

--- Graphic Monitoring_TXT/Form1.Designer.cs
+++ Graphic Monitoring_TXT/Form1.Designer.cs
@@ -28,27 +28,292 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tb_startTime = new System.Windows.Forms.TextBox();
+            this.tb_winlen = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tb_moteid = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btn_start = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.tb_src = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.tb_speed = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.tb_x_pos = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.tb_y_pos = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.cb_type = new System.Windows.Forms.ComboBox();
+            this.btn_stop = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.groupBox2.SuspendLayout();
             this.SuspendLayout();
             // 
             // groupBox1
             // 
+            this.groupBox1.Controls.Add(this.chart1);
             this.groupBox1.Location = new System.Drawing.Point(13, 13);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(1304, 607);
+            this.groupBox1.Size = new System.Drawing.Size(1800, 692);
             this.groupBox1.TabIndex = 0;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Graph";
+            // 
+            // chart1
+            // 
+            chartArea1.AxisY.Interval = 300D;
+            chartArea1.AxisY.Maximum = 3000D;
+            chartArea1.AxisY.Minimum = -3000D;
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(6, 20);
+            this.chart1.Name = "chart1";
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.chart1.Series.Add(series1);
+            this.chart1.Size = new System.Drawing.Size(1750, 666);
+            this.chart1.TabIndex = 0;
+            this.chart1.Text = "chart1";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.cb_type);
+            this.groupBox2.Controls.Add(this.label9);
+            this.groupBox2.Controls.Add(this.tb_y_pos);
+            this.groupBox2.Controls.Add(this.label8);
+            this.groupBox2.Controls.Add(this.tb_x_pos);
+            this.groupBox2.Controls.Add(this.label7);
+            this.groupBox2.Controls.Add(this.tb_speed);
+            this.groupBox2.Controls.Add(this.label6);
+            this.groupBox2.Controls.Add(this.button2);
+            this.groupBox2.Controls.Add(this.tb_src);
+            this.groupBox2.Controls.Add(this.label5);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.tb_moteid);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Controls.Add(this.tb_winlen);
+            this.groupBox2.Controls.Add(this.tb_startTime);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Location = new System.Drawing.Point(19, 712);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(953, 138);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Setting";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(18, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(59, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "시작 시간";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(363, 87);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(83, 12);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "위도우 사이즈";
+            // 
+            // tb_startTime
+            // 
+            this.tb_startTime.Location = new System.Drawing.Point(101, 29);
+            this.tb_startTime.Name = "tb_startTime";
+            this.tb_startTime.Size = new System.Drawing.Size(241, 21);
+            this.tb_startTime.TabIndex = 2;
+            // 
+            // tb_winlen
+            // 
+            this.tb_winlen.Location = new System.Drawing.Point(452, 84);
+            this.tb_winlen.Name = "tb_winlen";
+            this.tb_winlen.Size = new System.Drawing.Size(85, 21);
+            this.tb_winlen.TabIndex = 3;
+            this.tb_winlen.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(181, 87);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(59, 12);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "이동 속도";
+            // 
+            // tb_moteid
+            // 
+            this.tb_moteid.Location = new System.Drawing.Point(101, 84);
+            this.tb_moteid.Name = "tb_moteid";
+            this.tb_moteid.Size = new System.Drawing.Size(67, 21);
+            this.tb_moteid.TabIndex = 5;
+            this.tb_moteid.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(1143, 64);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(17, 12);
+            this.label4.TabIndex = 6;
+            this.label4.Text = " s";
+            // 
+            // btn_start
+            // 
+            this.btn_start.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btn_start.Location = new System.Drawing.Point(987, 713);
+            this.btn_start.Name = "btn_start";
+            this.btn_start.Size = new System.Drawing.Size(388, 138);
+            this.btn_start.TabIndex = 2;
+            this.btn_start.Text = "START";
+            this.btn_start.UseVisualStyleBackColor = true;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(375, 32);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(71, 12);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "데이터 위치";
+            // 
+            // tb_src
+            // 
+            this.tb_src.Location = new System.Drawing.Point(452, 29);
+            this.tb_src.Name = "tb_src";
+            this.tb_src.Size = new System.Drawing.Size(245, 21);
+            this.tb_src.TabIndex = 8;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(703, 27);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(31, 23);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "...";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(18, 87);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(59, 12);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "모트 번호";
+            // 
+            // tb_speed
+            // 
+            this.tb_speed.Location = new System.Drawing.Point(246, 84);
+            this.tb_speed.Name = "tb_speed";
+            this.tb_speed.Size = new System.Drawing.Size(96, 21);
+            this.tb_speed.TabIndex = 11;
+            this.tb_speed.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(554, 87);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(71, 12);
+            this.label7.TabIndex = 12;
+            this.label7.Text = "x축 값 위치";
+            // 
+            // tb_x_pos
+            // 
+            this.tb_x_pos.Location = new System.Drawing.Point(631, 84);
+            this.tb_x_pos.Name = "tb_x_pos";
+            this.tb_x_pos.Size = new System.Drawing.Size(66, 21);
+            this.tb_x_pos.TabIndex = 13;
+            this.tb_x_pos.Text = "2";
+            this.tb_x_pos.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(756, 87);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(71, 12);
+            this.label8.TabIndex = 14;
+            this.label8.Text = "y축 값 위치";
+            // 
+            // tb_y_pos
+            // 
+            this.tb_y_pos.Location = new System.Drawing.Point(833, 84);
+            this.tb_y_pos.Name = "tb_y_pos";
+            this.tb_y_pos.Size = new System.Drawing.Size(100, 21);
+            this.tb_y_pos.TabIndex = 15;
+            this.tb_y_pos.Text = "9";
+            this.tb_y_pos.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(756, 32);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(71, 12);
+            this.label9.TabIndex = 16;
+            this.label9.Text = "y축 값 유형";
+            // 
+            // cb_type
+            // 
+            this.cb_type.FormattingEnabled = true;
+            this.cb_type.Items.AddRange(new object[] {
+            "x",
+            "y",
+            "z",
+            "xy",
+            "xz",
+            "yz",
+            "xyz"});
+            this.cb_type.Location = new System.Drawing.Point(833, 27);
+            this.cb_type.Name = "cb_type";
+            this.cb_type.Size = new System.Drawing.Size(100, 20);
+            this.cb_type.TabIndex = 17;
+            // 
+            // btn_stop
+            // 
+            this.btn_stop.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btn_stop.Location = new System.Drawing.Point(1381, 711);
+            this.btn_stop.Name = "btn_stop";
+            this.btn_stop.Size = new System.Drawing.Size(388, 138);
+            this.btn_stop.TabIndex = 3;
+            this.btn_stop.Text = "STOP";
+            this.btn_stop.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1329, 632);
+            this.ClientSize = new System.Drawing.Size(1784, 862);
+            this.Controls.Add(this.btn_start);
+            this.Controls.Add(this.btn_stop);
+            this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.groupBox1);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Graphic Monitoring Tool";
             this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -56,6 +321,28 @@
         #endregion
 
         private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox tb_moteid;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox tb_winlen;
+        private System.Windows.Forms.TextBox tb_startTime;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btn_start;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox tb_src;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox tb_y_pos;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox tb_x_pos;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox tb_speed;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.ComboBox cb_type;
+        private System.Windows.Forms.Button btn_stop;
     }
 }
 
